1b1ad610d937aa98941876a3c5d73cd008bde0ac
navpreet <navpreetbatth2.nb@gmail.com>
2024-03-12 16:09:57 -0700

    initial commit

---
+++ Testing/Models/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Testing.Models
+{
+
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+
+    }
+}
